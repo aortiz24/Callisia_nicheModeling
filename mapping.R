88cbf4dca098afb7bd6049372f06f0b82d729225
Andrea Ortiz <aortiz1865@gmail.com>
2016-09-10 11:12:19 -0500

    updated plotting code

--- mapping.R
+++ mapping.R
@@ -20,14 +20,14 @@ tetraploid <- tetraploid[,c(3,2)]
 
 ## quick and dirty plot on map (could also plot points first and add map)
 US(xlim=c(-85,-77), ylim=c(26,37))
-points(diploid$lon, diploid$lat, col='orange', pch=20)
-points(tetraploid$lon, tetraploid$lat, col='red', pch=20)
+points(diploid$Longitude, diploid$Latitude, col='orange', pch=20)
+points(tetraploid$Longitude, tetraploid$Latitude, col='red', pch=20)
 
 ## a slightly more refined map (using built-in state outlines)
 southeast <- c("florida", "georgia", "north carolina", "south carolina")
 map(database="state", regions = southeast, interior=T, lwd=2)
-points(diploid$lon, diploid$lat, col='orange', pch=20, cex=2)
-points(tetraploid$lon, tetraploid$lat, col='red', pch=20, cex=2)
+points(diploid$Longitude, diploid$Latitude, col='orange', pch=20, cex=2)
+points(tetraploid$Longitude, tetraploid$Latitude, col='red', pch=20, cex=2)
 
 ## using US census shapefiles, save custom (best) shapefile for modeling later
 # download, unzip all state shapefiles to new directory
@@ -46,6 +46,6 @@ writeSpatialShape(SEstates, "shapefiles/SEstates")
 dir.create("figures")
 pdf(file="figures/mapping.pdf")
 map(SEstates)
-points(diploid$lon, diploid$lat, col='orange', pch=20, cex=2)
-points(tetraploid$lon, tetraploid$lat, col='red', pch=20, cex=2)
+points(diploid$Longitude, diploid$Latitude, col='orange', pch=20, cex=2)
+points(tetraploid$Longitude, tetraploid$Latitude, col='red', pch=20, cex=2)
 dev.off()
