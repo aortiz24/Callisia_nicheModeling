52238f32e6a03f5426792214dca365fe07615685
Andrea Ortiz <aortiz1865@gmail.com>
2017-04-11 12:53:09 -0500

    updated climate layers for predictors variable

--- maxent.R
+++ maxent.R
@@ -62,8 +62,8 @@ tdmean11 <- raster("layers/tdmean11.asc", crs=CRS)
 ## create stack of non-correlated layers (as determined by layerPrep.R)
 predictors0<- stack(ppt0)
 predictors1<- stack(ppt1,tmax1)
-predictors9<- stack(tmean9, vpdmin9)
-predictors11<- stack(tmin11, ppt11, vpdmin11)
+predictors9<- stack(tmean9, ppt9, vpdmin9)
+predictors11<- stack(tmean11, ppt11, vpdmax11, vpdmin11)
 
 # plot each layer individually
 plot(predictors0)
